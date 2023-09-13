9e710d78def31b19be72e2cbff3cc9f3a9c22983
jasonplato <wangxufeibsdbd@gmail.com>
2023-09-13 10:06:30 +0800

    fix manifestUrl

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import './index.css'
 import { TonConnectUIProvider } from '@tonconnect/ui-react'
 
 // this manifest is used temporarily for development purposes
-const manifestUrl = 'https://jasonplato.github.io/my-twa/public/tonconnect-manifest.json';
+const manifestUrl = 'https://jasonplato.github.io/my-twa/tonconnect-manifest.json';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
