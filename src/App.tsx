8386922862257774daa1f13473cfb8bcfb7c852e
jasonplato <wangxufeibsdbd@gmail.com>
2023-09-14 23:41:40 +0800

    try same

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,35 @@ import './App.css'
 import '@twa-dev/sdk';
 import { TonConnectButton } from '@tonconnect/ui-react';
 import { useTonConnect } from './hooks/useTonConnect';
+import { MainButton } from "@vkruglikov/react-telegram-web-app";
+import { BackButton } from '@twa-dev/sdk/react';
+
 // import { useSenderAddr } from './hooks/useSenderAddr';
-import { BackButton } from "@vkruglikov/react-telegram-web-app";
 // import { useCounterContract } from './hooks/useCounterContract';
 
 function App() {
   const { sender } = useTonConnect();
   // const { value, address, sendIncrement } = useCounterContract();
-
+  
   return (
     <div className='App'>
       <div className='Container'>
         <TonConnectButton />
-        <BackButton
-          onClick={() => {
-            console.log("test hello", sender);
-          }}
-        />
+        <div className='Card'>
+          <MainButton
+              text="CLICK ME"
+              onClick={() => {
+                console.log("test hello", sender);
+              }}
+          />
+        </div>
+        <div className='Card'>
+          <BackButton
+              onClick={() => {
+                console.log("test hello");
+              }}
+          />
+        </div>
         {/* <div className='Card'>
           <b>Counter Address</b>
           <div className='Hint'>{address?.slice(0, 30) + '...'}</div>
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,23 @@
-
+<head>
+  
+</head>
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { TonConnectUIProvider } from '@tonconnect/ui-react'
+import { WebAppProvider } from "@vkruglikov/react-telegram-web-app";
 
 // this manifest is used temporarily for development purposes
 const manifestUrl = 'https://jasonplato.github.io/my-twa/tonconnect-manifest.json';
-
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <TonConnectUIProvider manifestUrl={manifestUrl}>
-    <App />
-  </TonConnectUIProvider>,
+  <WebAppProvider
+    options={{
+      smoothButtonsTransition: true,
+    }}
+  >
+    <TonConnectUIProvider manifestUrl={manifestUrl}>
+      <App />
+    </TonConnectUIProvider>
+  </WebAppProvider>,
+  
 )
